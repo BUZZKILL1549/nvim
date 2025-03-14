dcd5f535a9d457065aabc332b84c9c59f396c7b0
BUZZKILL1549 <aushvs10@hotmail.com>
2025-03-14 13:32:24 +0530

    Persistence

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -14,3 +14,9 @@ vim.keymap.set("n", "<leader>q", ":BufferClose<CR>", { desc = "Close Buffer", si
 -- telescope
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files", silent = true })
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live Grep", silent = true })
+
+-- persistence
+vim.keymap.set("n", "<leader>qs", function() require('persistence').load() end, { desc = "Restore Session" })
+vim.keymap.set("n", "<leader>qS", function() require('persistence').select() end, { desc = "Select Session" })
+vim.keymap.set("n", "<leader>ql", function() require('persistence').load({ last = true }) end, { desc = "Restore Last Session" })
+vim.keymap.set("n", "<leader>qd", function() require('persistence').stop() end, { desc = "Don't Save Current Session" })
